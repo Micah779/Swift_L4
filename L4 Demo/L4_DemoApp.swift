37175c0bab827fee3ab535098a9771ad35054f3c
Micah779 <contactmaldrich@gmail.com>
2023-09-22 20:51:48 -0400

    Initial Commit

---
+++ L4 Demo/L4_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L4_DemoApp.swift
+//  L4 Demo
+//
+//  Created by Micah Aldrich on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct L4_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
